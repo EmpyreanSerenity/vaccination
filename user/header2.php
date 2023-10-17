b03b9dede8597aa54d757aa7db8169737603ff2e
EmpyreanSerenity <yaseenpn22@gmail.com>
2023-10-17 23:22:29 +0530

    Sixth Commit

--- user/header2.php
+++ user/header2.php
@@ -86,6 +86,7 @@ $a=$_SESSION['email'];
 							<button class="menu-toggle"><i class="fa fa-bars"></i></button>
 							<ul class="menu">
 								<li class="home menu-item"><a href="index2.php"><img src="images/home-icon.png" alt="Home"></a></li>
+								<li class="menu-item"><a href="addchild.php">Add Child </a></li>
 								<li class="menu-item"><a href="contactus1.php">Contact us</a></li>
 								<li class="menu-item"><a href="index.php">Parent Logout</a></li>
 							
